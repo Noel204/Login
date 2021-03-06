8dc5dd8e267a22c6c1964d2ecde1e9770e6d81bc
Noel <Noel@DESKTOP-0FQ9JN3>
2021-03-05 22:44:02 -0600

    Login

---
+++ src/login/Login.java
@@ -0,0 +1,21 @@
+/*
+Noel Franciscoco Garcia Trejo
+Matricula: 2022580
+
+*/
+package login;
+
+/**
+ *
+ * @author Noel
+ */
+public class Login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
